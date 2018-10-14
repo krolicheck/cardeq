b48a24cb8504c3faad8fa9fa2b1b031e7544c76c
suvorovnn <suvorovnn@mail.ru>
2018-10-14 19:16:21 +0300

    Test project

---
+++ www.js
@@ -0,0 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var www = express();
+
+// view engine setup
+www.set('views', path.join(__dirname, 'views'));
+www.set('view engine', 'pug');
+
+www.use(logger('dev'));
+www.use(express.json());
+www.use(express.urlencoded({ extended: false }));
+www.use(cookieParser());
+www.use(express.static(path.join(__dirname, 'public')));
+
+www.use('/', indexRouter);
+www.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+www.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+www.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = www;
