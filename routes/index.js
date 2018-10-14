5820173bf0392bcc56c822f49065c6941f55b4da
suvorovnn <suvorovnn@mail.ru>
2018-10-14 19:40:56 +0300

    Test project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var aloha = {};
+  aloha.funya = function(){
+    console.log('funechka');
+  };
+  aloha.funya();
+  aloha.alohan = 'huy';
+  aloha.strength = '11';
+  res.render('index', { title: 'Express', aloha : JSON.stringify(aloha) });
 });
 
 module.exports = router;
